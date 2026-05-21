7dd7ec4361970758c5ee8e6e4fa97e336e4d6b86
francisco-oromi <francisco.oromi@gmail.com>
2026-05-21 18:19:50 -0300

    feat: ativa Google Tag Manager GTM-KF4528K2

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,8 +105,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${poppins.variable} ${inter.variable}`}>
       <head>
-        {/* Google Tag Manager — substitua GTM-XXXXXXX pelo seu ID */}
-        {/* <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-XXXXXXX');` }} /> */}
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KF4528K2');` }} />
         {/* Facebook Pixel — substitua 0000000000000 pelo seu ID */}
         {/* <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','0000000000000');fbq('track','PageView');` }} /> */}
         <script
@@ -115,8 +115,8 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-screen flex flex-col antialiased">
-        {/* GTM noscript — substitua GTM-XXXXXXX */}
-        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX" height="0" width="0" style={{display:"none",visibility:"hidden"}} /></noscript> */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KF4528K2" height="0" width="0" style={{display:"none",visibility:"hidden"}} /></noscript>
         <LeadFormProvider>
           <Header />
           <main className="flex-1 pt-[88px] pb-[64px] md:pb-0">{children}</main>
